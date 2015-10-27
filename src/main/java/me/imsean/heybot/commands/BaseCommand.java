2ec7946cd4cc8f145e30d67aa285685a1c2e859a
Matthew Steglinski <matthew.steglinski@utoronto.ca>
2015-10-27 19:09:44 -0400

    Remove unnecessary command name check

--- src/main/java/me/imsean/heybot/commands/BaseCommand.java
+++ src/main/java/me/imsean/heybot/commands/BaseCommand.java
@@ -17,40 +17,38 @@ public class BaseCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String command, String[] args) {
-        if (command.equalsIgnoreCase("heybot")) {
-            if (args.length == 0) {
-                sender.sendMessage(plugin.colorize("&6HeyBot by killazombiecow (http://imsean.me)"));
+        if (args.length == 0) {
+            sender.sendMessage(plugin.colorize("&6HeyBot by killazombiecow (http://imsean.me)"));
+        }
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("help")) {
+                if (sender.hasPermission("heybot.admin")) {
+                    sender.sendMessage(plugin.colorize("&7-----&8[&6HeyBot Commands&8]&7-----"));
+                    sender.sendMessage(plugin.colorize("&8/heybot reload &7- Reload heybot's configuration"));
+                    sender.sendMessage(plugin.colorize("&8/heybot blacklist (add/remove) <username> &7- Add a player to blacklist"));
+                } else {
+                    sender.sendMessage(plugin.colorize("&cInsufficient permissions."));
+                }
             }
-            if (args.length > 0) {
-                if (args[0].equalsIgnoreCase("help")) {
-                    if (sender.hasPermission("heybot.admin")) {
-                        sender.sendMessage(plugin.colorize("&7-----&8[&6HeyBot Commands&8]&7-----"));
-                        sender.sendMessage(plugin.colorize("&8/heybot reload &7- Reload heybot's configuration"));
-                        sender.sendMessage(plugin.colorize("&8/heybot blacklist (add/remove) <username> &7- Add a player to blacklist"));
-                    } else {
-                        sender.sendMessage(plugin.colorize("&cInsufficient permissions."));
-                    }
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission("heybot.admin")) {
+                    plugin.reloadConfig();
+                    plugin.saveConfig();
+                    sender.sendMessage(plugin.colorize("&6HeyBot configuration reloaded!"));
+                } else {
+                    sender.sendMessage(plugin.colorize("&cInsufficient permissions."));
                 }
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (sender.hasPermission("heybot.admin")) {
-                        plugin.reloadConfig();
-                        plugin.saveConfig();
-                        sender.sendMessage(plugin.colorize("&6HeyBot configuration reloaded!"));
-                    } else {
-                        sender.sendMessage(plugin.colorize("&cInsufficient permissions."));
-                    }
+            }
+            if (args[0].equalsIgnoreCase("blacklist")) {
+                if (true) {
+                    sender.sendMessage(plugin.colorize("&6This feature isn't available yet!"));
+                    return false;
                 }
-                if (args[0].equalsIgnoreCase("blacklist")) {
-                    if (true) {
-                        sender.sendMessage(plugin.colorize("&6This feature isn't available yet!"));
+                if (args[1].equalsIgnoreCase("add")) {
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) {
+                        sender.sendMessage(plugin.colorize("&cPlease specify a player"));
                         return false;
-                    }
-                    if (args[1].equalsIgnoreCase("add")) {
-                        Player target = Bukkit.getPlayer(args[1]);
-                        if (target == null) {
-                            sender.sendMessage(plugin.colorize("&cPlease specify a player"));
-                            return false;
-                        }
                     }
                 }
             }
