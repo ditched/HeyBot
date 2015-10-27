ce76c12aa0a9b54541f0547391d81e7c1664ba6b
Matthew Steglinski <matthew.steglinski@utoronto.ca>
2015-10-27 18:54:29 -0400

    Mavenize

--- src/main/java/me/imsean/heybot/listeners/ChatListener.java
+++ src/main/java/me/imsean/heybot/listeners/ChatListener.java
@@ -1,12 +1,6 @@
 package me.imsean.heybot.listeners;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
 import me.imsean.heybot.HeyBot;
-
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,12 +8,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.Plugin;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 public class ChatListener implements Listener {
-	
+
 	Plugin plugin = HeyBot.getInstance();
 	String beginsWith = plugin.getConfig().getString("begins-with");
 	String prefix = plugin.getConfig().getString("bot-prefix");
-	
+
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
 		if(prefix == "none") {
@@ -29,9 +28,9 @@ public class ChatListener implements Listener {
 		String message = e.getMessage();
 		if(!message.startsWith(beginsWith)) return;
 		message = message.replace(beginsWith, "").trim();
-		
+
 		if(message.isEmpty()) return;
-		
+
 		final List<String> responses = plugin.getConfig().getStringList("messages." + message + ".responses");
 		if(responses.isEmpty()) return;
 		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
@@ -40,14 +39,14 @@ public class ChatListener implements Listener {
 			}
 		}, 1L);
 	}
-	
+
 	public String getRandomResponse(@SuppressWarnings("rawtypes") List list) {
 		Random random = new Random();
 		int idx = random.nextInt(list.size());
 		String response = list.get(idx).toString();
 		return response;
 	}
-	
+
 	public String formatResponse(String str, Player p) {
 		str = str.replace("%player name%", p.getName())
 				 .replace("%player display name%", p.getDisplayName())
